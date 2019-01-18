4ea342f5604c0cebeb0ae359be8d504293ed511c
Kevinrosales <kevinrosalesolvera@gmail.com>
2019-01-18 13:38:35 -0800

    finishing up for today

---
+++ src/main/java/com/kevinrosales/healthtrackerbackend/ExerciseController.java
@@ -0,0 +1,30 @@
+package com.kevinrosales.healthtrackerbackend;
+
+
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExerciseController {
+    @Autowired
+    private ExerciseRepository exerciseRepo;
+
+//    @PostMapping(value = "/exercises")
+//    public Exercise addExercise(@RequestParam String title, @RequestParam String quantity, @RequestParam String description) {
+//
+//    }
+
+    @GetMapping(value = "/exercises")
+    public String getExercises() {
+        Gson gson = new Gson();
+        String JsonExercise = gson.toJson(exerciseRepo.findAll());
+        return JsonExercise;
+    }
+
+
+
+}
