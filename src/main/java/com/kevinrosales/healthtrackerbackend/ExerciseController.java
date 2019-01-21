f900074746250e581cde118828527ff293d8f821
Kevinrosales <kevinrosalesolvera@gmail.com>
2019-01-21 10:43:53 -0800

    trying to set up controller

--- src/main/java/com/kevinrosales/healthtrackerbackend/ExerciseController.java
+++ src/main/java/com/kevinrosales/healthtrackerbackend/ExerciseController.java
@@ -1,6 +1,5 @@
 package com.kevinrosales.healthtrackerbackend;
 
-
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
